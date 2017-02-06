7a35eee1d3fdb5ca4e6113fed161ca105906f30f
didierclin <didier.c62@numericable.fr>
2017-02-06 16:46:35 +0100

    add test and factorisation for family

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -7,7 +7,22 @@ RSpec.describe Family, type: :model do
     expect(Family.attribute_names).to include("father_name", "mother_name")
 
   end
-  it "is invalid without mother or father name"
 
-  
+  it "is invalid without mother or father name long enough" do
+    family=Family.new
+    expect {family.valid?}.to(
+      change{family.errors[:father_name]}.from([]).
+      and change{family.errors[:mother_name]}.from []
+    )
+    [:father_name, :mother_name].each do |name|
+      family.send name.to_s+"=", "S"
+      family.valid?
+      expect(family.errors[name].select{|e| e.match "is too short"}).not_to be_empty
+      family.send name.to_s+"=", "Sy"
+      family.valid?
+      expect(family.errors[name].select{|e| e.match "is too short"}).to be_empty
+    end
+  end
+
+
 end
