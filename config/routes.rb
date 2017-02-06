db44fa5a61864d7fe0b915fd4ed17085159be888
patrick <pat.pbe@instanweb.fr>
2017-02-06 16:45:39 +0100

    add profile class

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :profiles, only: [:index, :show, :new, :create, :update]
   resources :families, only: [:show, :new, :create, :update]
 
   namespace :admin do
