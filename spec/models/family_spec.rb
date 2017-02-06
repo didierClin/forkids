a85f7000eaf132438776129b53b5624229976444
didierclin <didier.c62@numericable.fr>
2017-02-06 15:31:56 +0100

    add factory_girl initialisation - add family tests

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Family, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a father and a mother name" do
+    expect(Family.attribute_names).to include("father_name", "mother_name")
+
+  end
+  it "is invalid without mother or father name"
+
+  
 end
