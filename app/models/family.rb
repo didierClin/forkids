2741d934b022447a2cba67163b06ce5827350ae7
didierclin <didier.c62@numericable.fr>
2017-02-07 12:06:39 +0100

    add_habtm relations between profiles and profiles

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,3 +1,4 @@
 class Family < ApplicationRecord
   validates :father_name,:mother_name, presence: true, length: { minimum: 2 }
+  has_and_belongs_to_many :profiles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206151408) do
+ActiveRecord::Schema.define(version: 20170207101217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170206151408) do
     t.string   "mother_name"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "families_profiles", id: false, force: :cascade do |t|
+    t.integer "family_id"
+    t.integer "profile_id"
+    t.index ["family_id"], name: "index_families_profiles_on_family_id", using: :btree
+    t.index ["profile_id"], name: "index_families_profiles_on_profile_id", using: :btree
   end
 
   create_table "profiles", force: :cascade do |t|
