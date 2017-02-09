be35a51a57bc258698d036b25b73ad2d0f76deac
didierclin <didier.c62@numericable.fr>
2017-02-09 10:57:07 +0100

    change family_member helper

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,5 @@ class Profile < ApplicationRecord
   has_and_belongs_to_many :families
   belongs_to :user
 
-  scope :family_members, ->(profile) {where {}}
-
+  scope :family_members, ->(user) {joins(:families).where(families: { id: user.profile.families.first.id })}
 end
