b50543b4eb6ff1ab725c96dfda14c34472c6f550
didierclin <didier.c62@numericable.fr>
2017-02-10 10:40:32 +0100

    comment API routing (error) - add index method for administrate users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,14 +4,30 @@ module Admin
     # simply overwrite any of the RESTful actions. For example:
     #
     def index
-      super
-      if current_user.is_admin?
-        @resources = User.all.page(params[:page]).per(10)
-      else
-        @resources = current_user.family_members.page(params[:page]).per(10)
-      end
-    end
 
+      search_term = params[:search].to_s.strip
+
+      if current_user.is_admin?
+        resources = User.all
+      else
+        resources = User.all
+        # creer la requete de jointure
+        # f_id = current_user.profile.family_ids
+        # resources = User.profile.where(family_ids: f_id)
+        fail
+        # resources = User.find(current_user) #TODO
+      end
+
+      resources = order.apply(resources)
+      resources = resources.page(params[:page]).per(records_per_page)
+      page = Administrate::Page::Collection.new(dashboard, order: order)
+
+      render locals: {
+        resources: resources,
+        search_term: search_term,
+        page: page,
+      }
+    end
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
     #   User.find_by!(slug: param)
